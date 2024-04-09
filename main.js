b44f79866c158bf134733f79b6bcd1aea5b1165a
jayson <jaysonshammah3@gmail.com>
2024-04-09 20:08:19 +0300

    add change

---
+++ main.js
@@ -0,0 +1,23 @@
+const baseUrl = "https://dog.ceo/api/breeds";
+const randomImg = document.querySelector('#random');
+const rightSect = document.querySelector(`.right-section`);
+const explore = document.querySelector(`#explore`);
+
+
+console.log(randomImg);
+
+function fetchRandomDog() {
+    fetch(`${baseUrl}/image/random`)
+        .then((res) => res.json())
+        .then((data) => {
+            rightSect.innerHTML=`<img 
+                        src= ${data.message}
+                        alt="doggo" 
+                        id = "random"
+           />`;
+      });
+}
+
+explore.addEventListener('click', fetchRandomDog);
+
+fetchRandomDog();
