e5d4ecd1ac34bdb9c1af3978f1330f1485b06b63
jayson <jaysonshammah3@gmail.com>
2024-04-12 14:21:13 +0300

    add change

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const randomImg = document.querySelector('#random');
 const rightSect = document.querySelector(`.right-section`);
 const explore = document.querySelector(`#explore`);
 const select = document.querySelector(`#breeds`);
-
+const dogsDisplay = document.querySelector(`.dogs-display`);
 
 function init() {
     fetchRandomDog();
     fetchAllDogBreeds(); 
+    
 }
-
+    
 function fetchRandomDog() {
     fetch(`${baseUrl}/image/random`)
         .then((res) => res.json())
@@ -41,20 +42,30 @@ function fetchAllDogBreeds() {
 
             select.appendChild(option);
         });
+
+        select.addEventListener('change', (event) => {
+            const selectedValue = event.target.value;
+            
+            fetchDogBreed(selectedValue);
+        });
     });
 }
 function fetchDogBreed(breed) {
-    fetch (`https://dog.ceo/api/breed/${breed}`)
+    fetch (`https://dog.ceo/api/breed/${breed}/images`)
     .then(res => res.json())
     .then((data) => {
         const images = data.message.slice(0, 8);
+
         images.map((image) => {
             const dogCard = document.createElement('div');
             dogCard.className = 'dog-card';
 
             const dogImage = document.createElement('img');
             dogImage.src = image;
+            dogImage.alt = 'dog image';
             dogCard.appendChild(dogImage);
+
+            dogsDisplay.appendChild(dogCard);
             
         });
   });
